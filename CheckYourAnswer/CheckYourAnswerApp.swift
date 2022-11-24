61904c86080062729221f151bc060aa192b99855
C'est une pomme <dmgordienko@gmail.com>
2022-11-24 13:40:08 +0400

    Initial Commit

---
+++ CheckYourAnswer/CheckYourAnswerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckYourAnswerApp.swift
+//  CheckYourAnswer
+//
+//  Created by Дмитрий Гордиенко on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckYourAnswerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
